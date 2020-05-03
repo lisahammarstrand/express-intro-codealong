4467cb0ad752b185e84a6afd1ae45c46f96b2015
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-05-03 16:54:51 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from './data.json'
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,6 +18,26 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Get the whole data set
+app.get("/nominations", (req, res) => {
+  res.json(data)
+})
+
+// Get the nominations from a particular year. +year turnes string into number
+app.get("/year/:year", (req, res) => {
+  const year = req.params.year
+  const showWon = req.query.won
+
+  let nominationsFromYear = data.filter((item) => item.year_award === +year)
+
+  // Filter out the nominations that won that year
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+
+  res.json(nominationsFromYear)
 })
 
 // Start the server
